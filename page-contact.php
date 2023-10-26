62337f7b9323ba2ed8b29001012d276aa5b0ca87
Asunixyo <notoasuka0106@gmail.com>
2023-10-26 14:45:28 +0900

    contact修正

--- page-contact.php
+++ page-contact.php
@@ -8,9 +8,9 @@
 <body>
   <?php get_template_part('parts/header') ?>
   <main class="u-max-w-content u-mt5">
-    <h2 class="c-secondary-4-title u-text-center">Contact</h2>
-    <div class="u-m2">
-        <div><?php the_content(); ?></div>
+    <h2 class="c-secondary-4-title u-text-center u-mb4">Contact</h2>
+    <div class="u-m2-sm-md">
+      <?php the_content(); ?>
     </div>
   </main>
   <?php get_footer(); ?>
